ad422acd6478c3b3e95229ccf2fa099cb5ed8766
Rostislav Raykov <z@zbrox.org>
2013-10-29 15:52:20 +0100

    Initial Commit

---
+++ Task1Tests/Task1Tests.m
@@ -0,0 +1,34 @@
+//
+//  Task1Tests.m
+//  Task1Tests
+//
+//  Created by Rostislav Raykov on 10/29/13.
+//  Copyright (c) 2013 publicable. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Task1Tests : XCTestCase
+
+@end
+
+@implementation Task1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
